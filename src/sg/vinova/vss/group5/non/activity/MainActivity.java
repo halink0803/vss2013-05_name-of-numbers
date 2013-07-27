b5dc2a514a2b2cb0b4b101d36e3d20baeaba4889
halink0803 <halink0803@gmail.com>
2013-07-27 22:12:53 +0700

    define LoginActivity

--- src/sg/vinova/vss/group5/non/activity/MainActivity.java
+++ src/sg/vinova/vss/group5/non/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package sg.vinova.vss.group5.non.activity;
 
 import sg.vinova.vss.group5.non.R;
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,12 +14,55 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStop(){
+		super.onStop();
+	}
+	
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+	}
+	
+	public void StartLogIn(View v){
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+	}
+	
+	public void finishMainActivity(View v){
+		MainActivity.this.finish();
 	}
 
 }
