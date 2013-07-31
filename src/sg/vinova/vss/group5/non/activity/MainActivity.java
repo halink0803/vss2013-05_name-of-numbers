981be383dbcfccc7f038f4d39efe489ab800564d
halink0803 <halink0803@gmail.com>
2013-07-31 21:49:17 +0700

    SharePreferences

--- src/sg/vinova/vss/group5/non/activity/MainActivity.java
+++ src/sg/vinova/vss/group5/non/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package sg.vinova.vss.group5.non.activity;
 
 import sg.vinova.vss.group5.non.R;
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +15,12 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +28,71 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	String prefname="my_data";
+	
+	
+	public void check()
+	{
+		SharedPreferences pre=getSharedPreferences
+				(prefname,MODE_PRIVATE);
+			//lấy giá trị checked ra, nếu không thấy thì giá trị mặc định là false
+			boolean bchk=pre.getBoolean("checked", false);
+			
+			if (!bchk)
+			{
+				//StartLogIn(null);
+				Intent intent = new Intent(this, LoginActivity.class);
+				startActivity(intent);
+			}
+			else
+			{
+				//StartMainScreen(null);
+				Intent intent = new Intent(this, MainScreen.class);
+				startActivity(intent);
+			}
+	}
 
+	private void StartMainScreen(View v) {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this, MainScreen.class);
+		startActivity(intent);
+	}
+
+	public void StartLogIn(View v){
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStop(){
+		super.onStop();
+	}
+	
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+	}
+	
+	public void finishMainActivity(View v){
+		MainActivity.this.finish();
+	}
 }
---
+++ src/sg/vinova/vss/group5/non/activity/Tab3.java
@@ -0,0 +1,43 @@
+package sg.vinova.vss.group5.non.activity;
+
+import sg.vinova.vss.group5.non.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab3 extends Fragment{
+	
+	private TextView text;
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		//View v = LayoutInflater.from(getActivity()).inflate(R.layout.tab_layout,null);
+		View v = inflater.inflate(R.layout.tab_layout,container, false);
+		
+		text = (TextView) v.findViewById(R.id.text);
+		if (getArguments() != null) {
+			//
+			try {
+				//String value = getArguments().getString("key");
+				text.setText("Other Groups");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return v;
+		
+	}
+	
+	public void onActivityCreated(Bundle savedInstanceState){
+		super.onActivityCreated(savedInstanceState);
+	}
+}
