934e1abd96b1e6c6336a49f16e1e6f5a3fdc07a8
halink0803 <halink0803@gmail.com>
2013-08-01 00:53:30 +0700

    Complete SharedPreferences

--- src/sg/vinova/vss/group5/non/activity/MainActivity.java
+++ src/sg/vinova/vss/group5/non/activity/MainActivity.java
@@ -7,13 +7,52 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+//import android.view.View;
 
 public class MainActivity extends Activity {
+	
+	Button btnenter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnenter = (Button) findViewById(R.id.enter);
+		btnenter.setOnClickListener(
+				new View.OnClickListener() {
+			public void onClick(View arg0) {
+				check();
+			}
+		});
+	}
+	
+	String prefname="my_data";
+	public void check()
+	{
+		SharedPreferences pre=getSharedPreferences
+				(prefname,MODE_PRIVATE);
+			boolean bchk=pre.getBoolean("checked", false);
+			
+			if (!bchk)
+			{
+				StartLogIn();
+				
+				/*Intent intent = new Intent(this, LoginActivity.class);
+				startActivity(intent);*/
+				//tạo đối tượng Editor để lưu thay đổi
+				SharedPreferences.Editor editor=pre.edit();
+					//lưu vào editor
+					editor.putBoolean("checked", true);
+				//chấp nhận lưu xuống file
+				editor.commit();
+			}
+			else
+			{
+				StartMainScreen();
+				/*Intent intent = new Intent(this, MainScreen.class);
+				startActivity(intent);*/
+			}
 	}
 	
 	@Override
@@ -28,37 +67,16 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	String prefname="my_data";
-	
-	
-	public void check()
-	{
-		SharedPreferences pre=getSharedPreferences
-				(prefname,MODE_PRIVATE);
-			boolean bchk=pre.getBoolean("checked", false);
-			
-			if (!bchk)
-			{
-				//StartLogIn(null);
-				Intent intent = new Intent(this, LoginActivity.class);
-				startActivity(intent);
-			}
-			else
-			{
-				//StartMainScreen(null);
-				Intent intent = new Intent(this, MainScreen.class);
-				startActivity(intent);
-			}
-	}
 
-	/*private void StartMainScreen(View v) {
+	private void StartMainScreen() {
 		// TODO Auto-generated method stub
+		finish();
 		Intent intent = new Intent(this, MainScreen.class);
 		startActivity(intent);
-	}*/
+	}
 	
-	public void StartLogIn(View v){
+	public void StartLogIn(){
+		finish();
 		Intent intent = new Intent(this, LoginActivity.class);
 		startActivity(intent);
 	}
